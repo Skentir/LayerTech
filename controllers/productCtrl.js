96cd4129f6cb58c192ccee30fb357ebe084e3ad7
Dianne <dianne_chong@dlsu.edu.ph>
2020-09-21 11:11:53 +0800

    Modal Update

---
+++ controllers/productCtrl.js
@@ -0,0 +1,63 @@
+// Edit Function Templates For Edit Products
+
+const bcrypt = require('bcrypt');
+
+const userModel = require('../models/product');
+
+const {validationResult} = require('express-validator');
+
+
+exports.editProduct = (req, res, next) => {
+  
+  console.log(req.body);
+
+  // get user objects to validate password
+  // if match get one and update 
+  // else redirect to profile page with error message for wrong password
+
+//   _id : mongoose.Schema.Types.ObjectId,
+//   stockID :[{type:mongoose.Schema.Types.ObjectID}, ref:'stock'],
+//   productName : String,
+//   expirationDate : Date,
+//   dateAdded : Date,
+//   Quantity : Number,
+//   basePrice : Number,
+//   sellingPrice : Number
+
+  userModel.getAndUpdate(
+    { _id: req.session.product },
+    { productName: req.body.productName },
+    { new: true },
+    (err, result) => {
+      if (err) {
+        req.flash('error_msg', 'Something happened! Please try again.');
+        res.redirect('/inventory');
+      } else {
+        
+          if (product) {
+            bycrypt.compare(password, user.password, (err, result) => {
+              if (result) {
+                req.session.product = product._id;
+                req.session.productName = product.address;
+                res.status(200).json({
+                  message: "Product Name Successfully Updated",
+                })
+
+                res.redirect('/inventory');
+              } else {
+                req.flash('error_msg', 'Something went wrong!');
+                res.redirect('/inventory');
+              }
+            });
+          }
+
+      }
+      
+      console.log(result);
+      //if successful redirect to profile but send suc message
+      // res.status(200).json({
+      //    message: "Product Successfully Updated",
+      // })
+    }
+  )
+};
