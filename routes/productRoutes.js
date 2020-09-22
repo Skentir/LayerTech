e3bdc9c2d3ce54f7f3cd3862d52d6a1966bed362
Dianne <dianne_chong@dlsu.edu.ph>
2020-09-23 07:52:11 +0800

    testing read

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const productController = require('../controllers/productCtrl');
 
-
+router.get('/inventory', productController.getProduct)
 //router.get('/logout', userController.logoutUser);
 
 module.exports = router;
