f048fce63b490e548eea4cd99ce766fca0fead36
antonstaana <54714576+antonstaana@users.noreply.github.com>
2020-09-27 10:47:32 +0800

    Revert "Add Product Validation"
    This reverts commit c5d22ff2cd7be4c041b4466c2c56c0ad33846227.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+
 const productController = require('../controllers/productCtrl');
-const {addProductValidation} = require('../validators.js');
 
 router.get('/', productController.getProducts)
 router.get('/getItemDetails/:id', productController.getItemDetails)
 router.post('/updateItem/:id', productController.updateItem)
-router.post('/addProduct', addProductValidation, productController.addProduct)
+//router.get('/logout', userController.logoutUser);
+
+//Add product
+router.post('/addProduct', productController.addProduct)
 
 module.exports = router;
