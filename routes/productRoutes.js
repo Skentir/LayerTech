c5d22ff2cd7be4c041b4466c2c56c0ad33846227
antonstaana <54714576+antonstaana@users.noreply.github.com>
2020-09-27 10:02:24 +0800

    Add Product Validation
    Partial code for Add Product Validation: Modified validators.js and productRoutes.js to include addProductValidation

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
-
 const productController = require('../controllers/productCtrl');
+const {addProductValidation} = require('../validators.js');
 
 router.get('/', productController.getProducts)
 router.get('/getItemDetails/:id', productController.getItemDetails)
 router.post('/updateItem/:id', productController.updateItem)
-//router.get('/logout', userController.logoutUser);
-
-//Add product
-router.post('/addProduct', productController.addProduct)
+router.post('/addProduct', addProductValidation, productController.addProduct)
 
 module.exports = router;
