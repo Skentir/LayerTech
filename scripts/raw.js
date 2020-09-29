37f6fd87e6705485fd5d46735cbef8e1fde548b0
Kirsten S <daniellesison19@gmail.com>
2020-09-29 20:09:12 +0800

    Updated HBS modals and launch script

--- scripts/raw.js
+++ scripts/raw.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
   
-    
+    var itemID;
+
+  $(".launchUpdate").click(function(){
+      itemID = $(this).data('id');
+
+    $.ajax({
+          type: "GET",
+          url: "/raw/getStockDetails/"+itemID,
+        }).done(function(data) {
+            $("#updateRawMaterial").val(data.rawMaterial);
+            $("#updateQuantity").val(data.quantity);
+            $("#updateSupp").val(data.supplier);
+            $("#updateUnit").val(data.unit);
+            $("#updateCost").val(data.cost);
+            $("#updateLocation").val(data.location);
+            var parsedDate = new Date(data.dateBought);
+            var finalDate = parsedDate.getFullYear() + "-" + ("0" + (parsedDate.getMonth() + 1)).slice(-2) + "-" + ("0" + (parsedDate.getDay() + 1)).slice(-2);
+            $("#updateBought").val(finalDate);
+            parsedDate = new Date(data.expirationDate);
+            finalDate = parsedDate.getFullYear() + "-" + ("0" + (parsedDate.getMonth() + 1)).slice(-2) + "-" + ("0" + (parsedDate.getDay() + 1)).slice(-2);
+            $("#updateExpiry").val(finalDate);
+        }).fail(function(){
+            alert("Can't fetch this item.")
+        });
+  });
     
     
     
