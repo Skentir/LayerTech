ba5b54c1ac91547a6b2b89b98ed480db3697ff9c
Kirsten S <daniellesison19@gmail.com>
2020-09-29 20:17:00 +0800

    Added launchUpdate

--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const stockController = require('../controllers/stockCtrl');
 
 router.get('/', stockController.getStocks)
-router.get('/getStockDetails/:id', stockController.getItemDetails)
+router.get('/getStockDetails/:id', stockController.getStockDetails)
 //router.post('/updateItem/:id', productController.updateItem)
 //router.get('/getProducts', productController.getItemDetails)
 //Add product
