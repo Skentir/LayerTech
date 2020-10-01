9cb2cf6f0530587a31ea0f114c12741ec2146e52
Kirsten S <daniellesison19@gmail.com>
2020-10-02 05:23:22 +0800

    Fixed date issue

--- scripts/raw.js
+++ scripts/raw.js
@@ -226,10 +226,10 @@ $(document).ready(function(){
             $("#updateCost").val(data.cost);
             $("#updateLocation").val(data.location);
             var parsedDate = new Date(data.dateBought);
-            var finalDate = parsedDate.getFullYear() + "-" + ("0" + (parsedDate.getMonth() + 1)).slice(-2) + "-" + ("0" + (parsedDate.getDate())).slice(-2);
+            var finalDate = parsedDate.getFullYear() + "-" + ("0" + (parsedDate.getMonth() + 1)).slice(-2) + "-" + parsedDate.getDate();
             $("#updateBought").val(finalDate);
             parsedDate = new Date(data.expirationDate);
-            finalDate = parsedDate.getFullYear() + "-" + ("0" + (parsedDate.getMonth() + 1)).slice(-2) + "-" + ("0" + (parsedDate.getDate())).slice(-2);
+            finalDate = parsedDate.getFullYear() + "-" + ("0" + (parsedDate.getMonth() + 1)).slice(-2) + "-" + parsedDate.getDate();
             $("#updateExpiry").val(finalDate);
         }).fail(function(){
             alert("Can't fetch this item.")
